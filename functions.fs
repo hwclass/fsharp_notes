d5dff7a699797454b29b76ad70deda969adb945a
Barış Güler <harderworking.class@gmail.com>
2017-01-23 23:56:22 +0000

    Create functions.fs

---
+++ functions.fs
@@ -0,0 +1,19 @@
+(*Functions*)
+
+let squareFunction (input:int) =
+  let sq = input * input
+  sq
+;;
+ val squareFunction : input:int -> int
+
+squareFunction 5
+;;
+ val it : int = 25
+
+// speed calculation
+let speed (dist:float) = dist / 2.0;;
+ val speed : dist:float -> float
+
+speed 20.0;;
+ val it : float = 10.0
+Raw
