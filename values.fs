486637379ca9a31f9c925bd6d4257738ad3d7dce
Barış Güler <harderworking.class@gmail.com>
2017-01-23 23:59:00 +0000

    Create values.fs

---
+++ values.fs
@@ -0,0 +1,23 @@
+'a';;
+ val it : char = 'a'
+
+"abc";;
+ val it : string = "abc"
+
+10;;
+ val it : int = 10
+
+10.4;;
+ val it : float = 10.4
+
+val it : float = 10.4
+;;
+ Unexpected keyword 'val' in interaction at 3,0
+ parse error at 3,0
+
+let it : float = 10.4;;
+ val it : float = 10.4
+
+it
+;;
+ val it : float = 10.4
